1fa00da3c7e4c3d267304bd6cb764556c9156ce3
Regina Nurieva <regginurieva@gmail.com>
2018-10-19 12:50:50 -0700

    add query response for name,city,gender for each doctor

--- src/doctors.js
+++ src/doctors.js
@@ -4,7 +4,7 @@ export class Doctor {
   getDoctorByName(name) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=wa-seattle&skip=0&limit=10&apikey=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       //https://api.betterdoctor.com/2016-03-01/doctors?name=John&first_name=Smith&location=wa-seattle&user_location=wa-seattle&skip=0&limit=10&user_key=fd61530822982e5a3fa1649ea6c353b4
       request.onload = function() {
         if (this.status === 200) {
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Doctor } from './doctors';
 
 $(document).ready(function() {
 
@@ -12,18 +13,19 @@ $(document).ready(function() {
     let promise = allDoctors.getDoctorByName(searchDoctor);
 
     promise.then(function (response) {
-        let body = JSON.parse(response);
-        console.log(body);
+      let body = JSON.parse(response);
+      console.log(body);
 
-        $('.doctorsList').text(`Doctors in your area ${searchDoctor}:`);
-        for (let i = 0; i < body._embedded.events.length; i++) {
-            $('.doctorsList').append(`<br>${body.doctors[i].name}`);
-            //$('.doctorsList').append(`--- Date: ${body._embedded.events[i].dates.start.localDate}`);
+      $('.doctorsList').text(`Doctors in your area ${searchDoctor}:`);
+      for (let i = 0; i < body.data.length; i++) {
+        $('.doctorsList').append(`<br>${body.data[i].profile.first_name} `+`${body.data[i].profile.last_name}`+`<br> ${body.data[i].profile.gender}`+`<br>
+          ${body.data[i].practices[0].phones[0].number}<br>`+`${body.data[i].practices[0].visit_address.city}`);
+          //$('.doctorsList').append(`--- Date: ${body._embedded.events[i].dates.start.localDate}`);
         }
-    }, function (error) {
+      }, function (error) {
         $('.showErrors').text(`There was an error processing your request: ${error.message}`);
 
+      });
     });
-});
 
-});
+  });
