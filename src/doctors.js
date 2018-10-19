e4fdc8113073f80f79ff58dda8898c92ec1a0649
Regina Nurieva <regginurieva@gmail.com>
2018-10-19 15:36:39 -0700

    clean up the code

--- src/doctors.js
+++ src/doctors.js
@@ -5,7 +5,6 @@ export class Doctor {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
-      //https://api.betterdoctor.com/2016-03-01/doctors?name=John&first_name=Smith&location=wa-seattle&user_location=wa-seattle&skip=0&limit=10&user_key=fd61530822982e5a3fa1649ea6c353b4
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -23,7 +22,6 @@ export class Doctor {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=wa-seattle&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
-      //https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=wa-seattle&skip=0&limit=10&apikey=${process.env.exports.apiKey}
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
